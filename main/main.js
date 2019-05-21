9504e8dd2887bda7dc6e2f724e287819a07aef67
MikeoPerfect <hfuboys@outlook.com>
2019-05-21 21:19:27 +0800

    thoughtworks.homework

--- main/main.js
+++ main/main.js
@@ -2,3 +2,35 @@ module.exports = function main() {
     console.log("Debug Info");
     return 'Hello World!';
 };
+module.exports = function main(str) {
+    const lcd_dict = {
+        "0": ["._.", "|.|", "|_|"],
+        "1": ["...", "..|", "..|"],
+        "2": ["._.", "._|", "|_."],
+        "3": ["._.", "._|", "._|"],
+        "4": ["...", "|_|", "..|"],
+        "5": ["._.", "|_.", "._|"],
+        "6": ["._.", "|_.", "|_|"],
+        "7": ["._.", "..|", "..|"],
+        "8": ["._.", "|_|", "|_|"],
+        "9": ["._.", "|_|", "..|"]
+    };
+
+    var nums = str.split('');
+    var lcd_buff = [];
+    for(var i=0;i<nums.length;i++){
+        lcd_buff.push(lcd_dict[nums[i]]);
+    }
+
+    var output = '';
+    for(var i=0;i<3;i++){
+        var output_buff=[];
+        for(var j=0;j<nums.length;j++){
+            output_buff.push(lcd_buff[j][i]);
+        }
+        output_buff = output_buff.join(' ');
+        output = output+output_buff+'\n';
+    }
+
+    return output;
+};
